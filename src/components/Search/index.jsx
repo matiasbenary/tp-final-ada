48db678305767d386a9db0b245703a648cdb0f0c
Matias Benary <matiasbenary@gmail.com>
2020-08-17 11:52:17 -0300

    :pencil: Buscador en el header funciona

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { useHistory } from "react-router-dom";
 
 const Search = () => {
+  const [value, setValue] = useState("");
+  const history = useHistory();
+
+  const handleChange = (event) => setValue(event.target.value);
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13 && value) {
+      history.push(`/multi/${value}/page/1`);
+    }
+  };
+
   return (
     <div className="search">
-      <input className="search--input" type="text" placeholder="Busqueda ..." />
+      <input
+        className="search--input"
+        type="text"
+        placeholder="Busqueda ..."
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
       <FiSearch className="search--icon" />
     </div>
   );
